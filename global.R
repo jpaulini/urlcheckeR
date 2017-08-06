37b1e4b7f7eb5b959f2e5e22eb86914e6e7766f6
jpaulini <jpaulini@gmail.com>
2017-08-06 14:26:23 -0300

    added URL

--- global.R
+++ global.R
@@ -1,4 +1,6 @@
-#https://functionsurlcheck.azurewebsites.net/api/HttpTriggerJSGetLastEvents?code=mxNHHLjCzKkvAeuFEpcP65tXt3aE8O1VrROEoAWuZgWEwE5KrR52nQ==
+url <- "https://functionsurlcheck.azurewebsites.net/api/HttpTriggerJSGetLastEvents?code=mxNHHLjCzKkvAeuFEpcP65tXt3aE8O1VrROEoAWuZgWEwE5KrR52nQ=="
+httr::GET(url)
 # Params:
 # prevDays
 # partition 
+data <- fromJSON(url)
