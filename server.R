8ae0f68fc1ef32e5381a8757c51a5313121a8c86
jpaulini <jpaulini@gmail.com>
2017-08-22 21:16:35 -0300

    another omitted library :(

--- server.R
+++ server.R
@@ -8,6 +8,7 @@
 library(shiny)
 library("tidyverse")
 library("jsonlite")
+library("lubridate")
 
 url <- "https://functionsurlcheck.azurewebsites.net/api/HttpTriggerJSGetLastEvents?code=mxNHHLjCzKkvAeuFEpcP65tXt3aE8O1VrROEoAWuZgWEwE5KrR52nQ=="
 # Params:
